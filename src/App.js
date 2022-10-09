d6cfb1bf5583413cdccc141da091f278080083b3
CGJI58 <leemsk22@naver.com>
2022-10-10 01:05:40 +0900

    API 호출 버튼 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const BUTTON_INTERVAL = 20;
+
+  const [activateBtn, setActivateBtn] = useState(false);
+  const [coolDown, setCoolDown] = useState(0);
+
+  const onClickBtn = () => {
+    console.log("데이터 불러오는 중...");
+    controlBtn();
+  };
+
+  const controlBtn = () => {
+    setActivateBtn(true);
+
+    setCoolDown(BUTTON_INTERVAL);
+    const timer = setInterval(
+      () => setCoolDown((current) => current - 1),
+      1000
+    );
+
+    setTimeout(() => {
+      setActivateBtn(false);
+      clearInterval(timer);
+    }, BUTTON_INTERVAL * 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>버튼이 클릭된 후 {BUTTON_INTERVAL}초간 클릭할 수 없게 만들기</h2>
+      <button onClick={onClickBtn} disabled={activateBtn}>
+        Call API
+      </button>
+      <span>{coolDown}</span>
     </div>
   );
 }
